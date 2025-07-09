81f086d86e2abb9dfff1a266a7e4eeec505cfd6e
Ashwin <ashwinabraham96@gmail.com>
2025-07-10 00:13:42 +0100

    Add initial project setup with Vite, React, and TailwindCSS
    Include config files, dependencies, .gitignore, and README for a new
    Boxcutter project template using Vite with React and TypeScript support.

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
+let __filename = fileURLToPath(import.meta.url);
+let __dirname = path.dirname(__filename);
+
+// https://vite.dev/config/
+export default defineConfig({
+    plugins: [react(), tailwindcss()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
+});
